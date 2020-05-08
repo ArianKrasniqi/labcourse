e027aacb4e4b1642dedd6d373ec81dd688929e92
ArdianFejzulli <fejzulliardian@gmail.com>
2020-05-08 02:31:26 +0200

    login with jsonwebtoken and auth

--- Online Supermarket/Server-side/index.js
+++ Online Supermarket/Server-side/index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require('./models/user');
+const { auth } = require('./middleware/auth');
 
 // DB Connection
 mongoose.connect(config.mongoURI,
@@ -18,7 +19,19 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
 // http method
+app.get("/api/staff/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+});
+
 app.post('/api/staff/register', (req, res) => {
     const user = new User(req.body)
 
@@ -33,6 +46,36 @@ app.post('/api/staff/register', (req, res) => {
             success: true,
             data: userData
         })
+    })
+});
+
+app.post('/api/staff/login', (req, res) => {
+    //find email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({ 
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+        }    
+    //compare password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch) {
+            return res.json({ 
+                loginSuccess: false,
+                message: "Wrong password"
+            });
+        }        
+    })
+    //generate token
+    user.generateToken((err, user) => {
+        if(err) 
+            return res.status(400).send(err);
+        res.cookie("x_auth", user.token).status(200).json({
+            loginSuccess: true
+        })
+    })
+    
     })
 });
 
