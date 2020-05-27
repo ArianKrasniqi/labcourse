a716e509016fb0bd7e68afc89f8955f5b9c481fd
ArdianFejzulli <fejzulliardian@gmail.com>
2020-05-27 03:07:06 +0200

    AllProducts component - LoadMore button feature

--- Online Supermarket/admin/src/components/views/Products/AllProducts.js
+++ Online Supermarket/admin/src/components/views/Products/AllProducts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Button, Form, Row, Card, Col } from 'antd';
+import { Typography, Button, Row, Card, Col } from 'antd';
 import Axios from 'axios';
 import ImageSlider from '../../utils/ImageSlider';
 
@@ -9,21 +9,46 @@ const { Title } = Typography;
 
 function AllProducts() {
 
-    const [Products, setProducts] = useState([])
+    const [Products, setProducts] = useState([]);
+    const [Skip, setSkip] = useState(0);
+    const [Limit, setLimit] = useState(8);
+    const [PostSize, setPostSize] = useState(0);
 
-    useEffect(() => {   
-        Axios.post('/api/product/getProducts', )
-            .then(response => {
-                if(response.data.success) {
-                    setProducts(response.data.products);
+    useEffect(() => {  
 
-                    console.log(response.data.products);
-                } else {
-                    alert('Gabime ne marrjen e te dhenave');
-                }
-            })
+        const variables = {
+            skip: Skip,
+            limit: Limit,
+        }
 
+        getProducts(variables)
     }, [])
+
+    const getProducts = (variables) => {
+        Axios.post('/api/product/getProducts', variables )
+        .then(response => {
+            if(response.data.success) {
+                setProducts(response.data.products);
+
+                setPostSize(response.data.postSize)
+            } else {
+                alert('Gabime ne marrjen e te dhenave');
+            }
+        })
+    }
+
+    const onLoadMore = () => {
+        let skip = Skip + Limit;
+
+        const variables = {
+            skip: skip,
+            limit: Limit,
+        }
+
+        getProducts(variables)
+
+        setSkip(skip)
+    }
 
     const renderCards = Products.map((product, index) => {
         
@@ -35,19 +60,17 @@ function AllProducts() {
                 <Meta
                     title={product.title}
                     description={product.description}
+                    description={`${product.price} euro`}
                 />
             </Card>
         </Col>
     }) 
-
 
     return (
         <div style={{ maxWidth: '700px', margin: '30px auto'}}>
             <div style={{ textAlign: 'center', marginBottom: '30px' }}>
                 <Title level={2}>Te gjitha produktet</Title>
             </div>
-            
-            <Form>
                 <Button style={{ marginRight: '115px'}} href='/products'>SHTO PRODUKTE</Button> 
                 <Button style={{ marginRight: '60px'}} href='/manageproduct'>MENAXHO PRODUKTET</Button>
                 <Button style={{ marginLeft: '70px'}} href='/categories'>SHTO KATEGORI</Button>
@@ -71,17 +94,18 @@ function AllProducts() {
                     </div>
                     :
                     <div >
-                        <Row gutter={[ 16,16 ]}>
+                        <Row gutter={[ 16, 16 ]}>
                             {renderCards}
                         </Row>
                     </div>
                 }
                 <br />
                 <br />
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <button>Shiko me shume</button>
-                </div>
-            </Form>
+                {PostSize >= Limit && 
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <button onClick={onLoadMore}>Shiko me shume</button>
+                    </div>                    
+                }
         </div>
     )
 }
